ef58241ed7223222aeffe1a3f9844622a52ed542
DuneSciFye <65089425+DuneSciFye@users.noreply.github.com>
2024-03-28 20:31:31 -0400

    Added more placeholders

--- src/main/java/net/sivils/stringutils/StringUtils.java
+++ src/main/java/net/sivils/stringutils/StringUtils.java
@@ -50,21 +50,19 @@ public final class StringUtils extends PlaceholderExpansion {
                         }
                     return "";
                 }
-                return "Incorrect argument amount.";
-            case "random":
+                return "Incorrect argument amount. Input: " + length;
+            case "randomint":
                 arguments = "_" + arguments;
-                if (!arguments.contains("_type:")||((arguments.contains("_type:int")||arguments.contains("_type:double"))&&(!arguments.contains("_min:")||!arguments.contains("_max:")))||(arguments.contains("_type:string")&&!arguments.contains("_text:"))){
+                if (!arguments.contains("_min:")||!arguments.contains("_max:")){
                     return "Invalid arguments.";
                 }
-                String regex = "(_seed:|_text:|_round:|_separator:|_min:|_max:|_type:)(.+?)(?=(_seed:|_text:|_round:|_separator:|_min:|_max:|_type:|$))";
+                String regex = "(_seed:|_min:|_max:)(.+?)(?=(_seed:|_min:|_max:|$))";
 
                 Pattern pattern = Pattern.compile(regex);
                 Matcher matcher = pattern.matcher(arguments);
 
-                String type = null, text = null, separator = ",";
-                double min = 0, max = 0;
-                long seed = 0;
-                int round = 2;
+                int min = 0, max = 0;
+                long seedInt = 0;
 
                 while (matcher.find()) {
                     String key = matcher.group(1);
@@ -72,16 +70,79 @@ public final class StringUtils extends PlaceholderExpansion {
 
                     switch (key) {
                         case "_seed:":
-                            seed = Long.parseLong(value);
+                            seedInt = Long.parseLong(value);
                             break;
                         case "_min:":
-                            min = Double.parseDouble(value);
+                            min = Integer.parseInt(value);
                             break;
                         case "_max:":
-                            max = Double.parseDouble(value);
+                            max = Integer.parseInt(value);
                             break;
-                        case "_type:":
-                            type = value;
+                    }
+                }
+                Random random = new Random();
+                if (seedInt!=0){
+                    random = new Random(seedInt);
+                }
+                return String.valueOf(random.nextInt((max - min + 1)) + min);
+            case "randomdouble":
+                arguments = "_" + arguments;
+                if (!arguments.contains("_min:")||!arguments.contains("_max:")){
+                    return "Invalid arguments. Missing min or max.";
+                }
+                String regexDouble = "(_seed:|_round:|_min:|_max:)(.+?)(?=(_seed:|_round:|_min:|_max:|$))";
+
+                Pattern patternDouble = Pattern.compile(regexDouble);
+                Matcher matcherDouble = patternDouble.matcher(arguments);
+
+                double minDouble = 0, maxDouble = 0;
+                long seedDouble = 0;
+                int round = 2;
+
+                while (matcherDouble.find()) {
+                    String key = matcherDouble.group(1);
+                    String value = matcherDouble.group(2);
+
+                    switch (key) {
+                        case "_seed:":
+                            seedDouble = Long.parseLong(value);
+                            break;
+                        case "_min:":
+                            minDouble = Double.parseDouble(value);
+                            break;
+                        case "_max:":
+                            maxDouble = Double.parseDouble(value);
+                            break;
+                        case "_round:":
+                            round = Integer.parseInt(value);
+                            break;
+                    }
+                }
+                Random randomDouble = new Random();
+                if (seedDouble!=0){
+                    randomDouble = new Random(seedDouble);
+                }
+                return String.format("%." + round + "f", randomDouble.nextDouble((maxDouble - minDouble)) + minDouble);
+            case "randomstring":
+                arguments = "_" + arguments;
+                if (!arguments.contains("_text:")){
+                    return "Invalid arguments. Missing text.";
+                }
+                String regexString = "(_seed:|_text:|_separator:)(.+?)(?=(_seed:|_text:|_separator:|$))";
+
+                Pattern patternString = Pattern.compile(regexString);
+                Matcher matcherString = patternString.matcher(arguments);
+
+                String text = null, separator = ",";
+                long seedString = 0;
+
+                while (matcherString.find()) {
+                    String key = matcherString.group(1);
+                    String value = matcherString.group(2);
+
+                    switch (key) {
+                        case "_seed:":
+                            seedString = Long.parseLong(value);
                             break;
                         case "_text:":
                             text = value;
@@ -89,25 +150,57 @@ public final class StringUtils extends PlaceholderExpansion {
                         case "_separator:":
                             separator = value;
                             break;
-                        case "_round:":
-                            round = Integer.parseInt(value);
+                    }
+                }
+                Random randomString = new Random();
+                if (seedString!=0){
+                    randomString = new Random(seedString);
+                }
+                String[] splitRandom = text.split(separator);
+                return splitRandom[randomString.nextInt(splitRandom.length)];
+            case "changecolor":
+                arguments = "_" + arguments;
+                if (!arguments.contains("_input:")||!arguments.contains("_newcolor:")){
+                    return "Invalid arguments. Requires input and newcolor.";
+                }
+                String regexChangeColor = "(_input:|_newcolor:)(.+?)(?=(_input:|_newcolor:|$))";
+
+                Pattern patternChangeColor = Pattern.compile(regexChangeColor);
+                Matcher matcherChangeCOlor = patternChangeColor.matcher(arguments);
+
+                String input = "", newcolor = "";
+
+                while (matcherChangeCOlor.find()) {
+                    String key = matcherChangeCOlor.group(1);
+                    String value = matcherChangeCOlor.group(2);
+
+                    switch (key) {
+                        case "_input:":
+                            input = value;
+                            break;
+                        case "_newcolor:":
+                            newcolor = value;
                             break;
                     }
                 }
-                Random random = new Random();
-                if (seed!=0){
-                    random = new Random(seed);
+                if (input.contains("_CONCRETE_POWDER")){
+                    return newcolor + input.substring(input.length() - 16);
+                } else if (input.contains("_CONCRETE")){
+                    return newcolor + input.substring(input.length() - 9);
+                } else if (input.contains("_STAINED_GLASS_PANE")){
+                    return newcolor + input.substring(input.length() - 19);
+                } else if (input.contains("_WOOL")){
+                    return newcolor + input.substring(input.length() - 5);
+                } else if (input.contains("_GlAZED_TERRACOTTA")){
+                    return newcolor + input.substring(input.length() - 18);
+                } else if (input.contains("_TERRACOTTA")){
+                    return newcolor + input.substring(input.length() - 11);
+                } else if (input.contains("_STAINED_GLASS")){
+                    return newcolor + input.substring(input.length() - 14);
+                } else if (input.contains("_CARPET")){
+                    return newcolor + input.substring(input.length() - 7);
                 }
-                if (Objects.equals(type, "string")){
-                    String[] splitRandom = text.split(separator);
-                    return splitRandom[random.nextInt(splitRandom.length)];
-                } else if (type.equalsIgnoreCase("int")){
-                    return String.valueOf(random.nextInt((int) (max - min + 1)) + (int)min);
-                } else if (type.equalsIgnoreCase("double")){
-                    return String.format("%." + round + "f", random.nextDouble((max - min)) + min);
-                } else {
-                    return null;
-                }
+
         }
 
 
